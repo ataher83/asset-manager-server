702e33a95b013c722a34750f1198e39735b3ea63
ataher83 <taher83@gmail.com>
2024-06-03 14:31:42 +0600

    mongodb setup & configure

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
 
 
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0yjrwty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 app.get('/', (req, res) => {
   res.send('Asset Manager is Running...')
