98d8163d710b7426dccc78692852e06ce3c308af
ataher83 <taher83@gmail.com>
2024-06-03 14:25:31 +0600

    server & port name updated

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 
-// middleware
+
+
 
 app.use(cors())
 app.use(express.json())
 
 
-
-
-
 app.get('/', (req, res) => {
-  res.send('Asset Manager Server is Running')
+  res.send('Asset Manager is Running...')
 })
 
 
 app.listen(port, () => {
-  console.log(`Asset Manager Server is Running on port ${port}`)
+  console.log(`Asset Manager is Running on port ${port}`)
 })
