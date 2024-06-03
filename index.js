d14f059c178979cad3cfa0b741854c24bc1f2e0a
ataher83 <taher83@gmail.com>
2024-06-03 14:43:09 +0600

    index.jsx updated & .env used to hide mongobd credentials

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
 const jwt = require('jsonwebtoken')
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
 
 
 app.use(cors())
